4cb21b7858a69f24af9d885deec74d5556d564ff
neoilu <deni5enko.i@yandex.ru>
2025-04-10 03:08:17 +0300

    fix: vite config build

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,5 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
